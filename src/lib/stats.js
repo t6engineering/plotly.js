39a8be90e0d8f0e2b4f0bb4fc64564d084a57d08
acxz <17132214+acxz@users.noreply.github.com>
2022-06-20 09:56:21 -0400

    add missing semicolon

--- src/lib/stats.js
+++ src/lib/stats.js
@@ -50,7 +50,7 @@ exports.mean = function(data, len) {
 exports.geometricMean = function(data, len) {
     if(!len) len = exports.len(data);
     return Math.pow(exports.aggNums(function(a, b) { return a * b; }, 1, data), 1 / len);
-}
+};
 
 exports.midRange = function(numArr) {
     if(numArr === undefined || numArr.length === 0) return undefined;
